d687b134978807ea7dd4689c0b35c930c10ec81b
Tyler Niccolls <tylerjniccolls@gmail.com>
2019-03-27 20:48:41 -0700

    minor fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,13 +5,6 @@ module.exports = function(app) {
         res.json(friendData);
       });
 
-// app.post("/api/friendlist", function(req, res) {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body parsing middleware
-//     friendData.push(req.body);
-//   });
-
 app.post("/api/friendlist", function(req, res) {
     friendData.push(req.body);
     res.json(true);
